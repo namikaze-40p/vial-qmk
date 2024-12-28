290948e0a8e19da25a80c97e4ed36e54f9672753
namikaze-40p <circle.edge.b@gmail.com>
2024-12-28 11:07:41 +0900

    Rename define, and add define for layer4.

--- keyboards/aki27/picot_o44/keymaps/vial-custom/keymap.c
+++ keyboards/aki27/picot_o44/keymaps/vial-custom/keymap.c
@@ -33,6 +33,8 @@ enum layer_names {
 enum custom_keycodes {
   PREV_HISTORY = SAFE_RANGE,
   NEXT_HISTORY,
+  PREV_SCREEN,
+  NEXT_SCREEN,
   MISSION_CONTROL,
   SHOW_DESKTOP,
 };
@@ -40,15 +42,17 @@ enum custom_keycodes {
 #define NUMS   MO(_NUMS)
 #define LOWER  MO(_LOWER)
 #define RAISE  MO(_RAISE)
+#define TRACK  MO(_TRACK)
 #define ADJUST MO(_ADJUST)
 
-#define FN0_ESC  LT(NUMS, KC_ESC)
-#define FN0_TGL  TG(NUMS)
-#define FN1_EN   LT(LOWER, KC_LNG2)
-#define FN2_JP   LT(RAISE, KC_LNG1)
-#define FN2_0    LT(RAISE, KC_0)
-#define FN3_EN   LT(ADJUST, KC_LNG2)
-#define FN3_JP   LT(ADJUST, KC_LNG1)
+#define L1_ESC  LT(NUMS, KC_ESC)
+#define L1_TGL  TG(NUMS)
+#define L2_EN   LT(LOWER, KC_LNG2)
+#define L3_JP   LT(RAISE, KC_LNG1)
+#define L3_0    LT(RAISE, KC_0)
+#define L4_BTN2 LT(TRACK, KC_BTN2)
+#define L5_EN   LT(ADJUST, KC_LNG2)
+#define L5_JP   LT(ADJUST, KC_LNG1)
 #define CTL_BKSP RCTL_T(KC_BSPC)
 #define SHT_MINS RSFT_T(KC_MINS)
 #define SHT_SPC  LSFT_T(KC_SPC)
@@ -56,44 +60,46 @@ enum custom_keycodes {
 
 #define PREV_H  PREV_HISTORY
 #define NEXT_H  NEXT_HISTORY
+#define PREV_S  PREV_SCREEN
+#define NEXT_S  NEXT_SCREEN
 #define M_CTRL  MISSION_CONTROL
 #define S_DESK  SHOW_DESKTOP
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT(
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   PREV_H,   NEXT_H,  KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,   KC_K,    KC_L,    CTL_BKSP,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                       KC_N,    KC_M,   KC_COMM, KC_DOT,  SHT_MINS,
-        FN0_ESC, KC_LALT, KC_LGUI, FN1_EN, SHT_SPC, KC_BTN1,  KC_BTN2, CTL_ENT, FN2_JP, KC_LEFT, KC_RGHT, KC_DEL
+        KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    XXXXXXX, SCRL_MO, KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,
+        KC_A,    KC_S,    KC_D,    KC_F,   KC_G,                       KC_H,    KC_J,  KC_K,    KC_L,    CTL_BKSP,
+        KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,                       KC_N,    KC_M,  KC_COMM, KC_DOT,  SHT_MINS,
+        L1_ESC,  KC_LALT, KC_LGUI, L2_EN,  SHT_SPC, KC_BTN1, L4_BTN2, CTL_ENT, L3_JP, KC_LEFT, KC_RGHT, KC_DEL
     ),
     [_NUMS] = LAYOUT(
         KC_TAB,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_7,  KC_8,    KC_9,   XXXXXXX,
-        KC_LCTL, XXXXXXX, FN0_TGL, KC_TAB,  XXXXXXX,                    XXXXXXX, KC_4,  KC_5,    KC_6,   KC_BSPC,
+        KC_LCTL, XXXXXXX, L1_TGL,  KC_TAB,  XXXXXXX,                    XXXXXXX, KC_4,  KC_5,    KC_6,   KC_BSPC,
         KC_LSFT, XXXXXXX, XXXXXXX, KC_LNG1, XXXXXXX,                    XXXXXXX, KC_1,  KC_2,    KC_3,   _______,
-        _______, _______, _______, FN1_EN,  _______, XXXXXXX, XXXXXXX, _______, FN2_0, KC_COMM, KC_DOT, _______
+        _______, _______, _______, L2_EN,   _______, XXXXXXX, XXXXXXX, _______, L3_0,  KC_COMM, KC_DOT, _______
     ),
     [_LOWER] = LAYOUT(
         KC_EXLM, KC_AT,  KC_HASH, KC_DLR,  KC_PERC, XXXXXXX, XXXXXXX, KC_CIRC, KC_AMPR, KC_ASTR, KC_SLSH, KC_QUES,
         KC_F1,   KC_F2,  KC_F3,   KC_F4,   KC_F5,                      KC_GRV,  KC_MINS, KC_EQL,  KC_SCLN, KC_QUOT,
         KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,                     KC_TILD, KC_UNDS, KC_PLUS, KC_COLN, KC_DQUO,
-        KC_F11,  KC_F12, _______, FN1_EN,  KC_SPC,  XXXXXXX, XXXXXXX, KC_ENT,  FN3_JP, KC_BSLS, KC_PIPE, _______
+        KC_F11,  KC_F12, _______, L2_EN,   KC_SPC,  XXXXXXX, XXXXXXX, KC_ENT,  L5_JP,   KC_BSLS, KC_PIPE, _______
     ),
     [_RAISE] = LAYOUT(
         KC_TAB,  KC_BTN1, KC_MS_U, KC_BTN2, KC_LPRN, XXXXXXX, XXXXXXX, KC_RPRN, KC_HOME, KC_UP,   KC_END,  XXXXXXX,
         KC_LCTL, KC_MS_L, KC_MS_D, KC_MS_R, KC_LBRC,                   KC_RBRC, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC,
         KC_LSFT, KC_WH_R, KC_BTN3, KC_WH_L, KC_LCBR,                   KC_RCBR, XXXXXXX, KC_PGUP, KC_PGDN, _______,
-        _______, KC_LALT, _______, FN3_EN,  KC_SPC,  XXXXXXX, XXXXXXX, KC_ENT,  FN2_JP,  KC_WH_D, KC_WH_U, _______
+        _______, KC_LALT, _______, L5_EN,   KC_SPC,  XXXXXXX, XXXXXXX, KC_ENT,  L3_JP,   KC_WH_D, KC_WH_U, _______
     ),
     [_TRACK] = LAYOUT(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, PREV_H,  SCRL_MO, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        KC_ESC,  XXXXXXX, XXXXXXX, SCRL_MO, KC_SPC,  KC_BTN1, KC_BTN2, KC_ENT,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, SCRL_MO, M_CTRL, S_DESK,  XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   PREV_H,  NEXT_H,  XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   PREV_S,  NEXT_S,  XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_ESC,  XXXXXXX, KC_LGUI, SCRL_MO, KC_SPC,  KC_BTN1, L4_BTN2, KC_ENT,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     ),
     [_ADJUST] = LAYOUT(
         QK_BOOT, _______, _______, _______, _______, XXXXXXX, XXXXXXX, _______, M_CTRL,  S_DESK,  _______, _______,
-        _______, _______, FN0_TGL, KC_CAPS, _______,                    _______, KC_VOLD, KC_VOLU, _______, _______,
+        _______, _______, L1_TGL,  KC_CAPS, _______,                    _______, KC_VOLD, KC_VOLU, _______, _______,
         _______, _______, _______, _______, _______,                    _______, KC_BRID, KC_BRIU, _______, _______,
         _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______
     ),
@@ -140,12 +146,13 @@ bool is_mouse_record_kb(uint16_t keycode, keyrecord_t* record) {
 
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case FN0_ESC:
-        case FN1_EN:
-        case FN2_JP:
-        case FN2_0:
-        case FN3_EN:
-        case FN3_JP:
+        case L1_ESC:
+        case L2_EN:
+        case L3_JP:
+        case L3_0:
+        case L4_BTN2:
+        case L5_EN:
+        case L5_JP:
         case CTL_BKSP:
         case SHT_MINS:
         case SHT_SPC:
@@ -172,6 +179,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 register_code(KC_LGUI);
                 tap_code(KC_RBRC);
                 unregister_code(KC_LGUI);
+            }
+            return false;
+        case PREV_S:
+            if (record->event.pressed) {
+                register_code(KC_LCTL);
+                tap_code(KC_COMM);
+                unregister_code(KC_LCTL);
+            }
+            return false;
+        case NEXT_S:
+            if (record->event.pressed) {
+                register_code(KC_LCTL);
+                tap_code(KC_DOT);
+                unregister_code(KC_LCTL);
             }
             return false;
         case M_CTRL:
