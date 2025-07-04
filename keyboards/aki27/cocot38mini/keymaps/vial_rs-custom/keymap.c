20c986392254699755ded5ad5ddfeaf45be23094
namikaze-40p <circle.edge.b@gmail.com>
2025-07-05 03:17:33 +0900

    cocot38mini - setup keymaps

--- keyboards/aki27/cocot38mini/keymaps/vial_rs-custom/keymap.c
+++ keyboards/aki27/cocot38mini/keymaps/vial_rs-custom/keymap.c
@@ -27,53 +27,111 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define COCOT_SCROLL_INV_DEFAULT false
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+enum layer_names {
+    _QWERTY,
+    _NUMS,
+    _LOWER,
+    _RAISE,
+    _TRACK,
+    _MACRO,
+    _ADJUST,
+};
 
-    [0] = LAYOUT(
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_ESC,  KC_H,    KC_J,    KC_K,    KC_L,    KC_MINS,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                          KC_LALT, KC_LNG2, KC_SPC,  MS_BTN1, KC_ENT,  KC_LNG1, KC_BSPC
+enum custom_keycodes {
+    PREV_HISTORY = SAFE_RANGE,
+    NEXT_HISTORY,
+    PREV_SCREEN,
+    NEXT_SCREEN,
+    MISSION_CONTROL,
+    SHOW_DESKTOP,
+    FULL_SCREEN,
+    CENTER_SCREEN,
+    LEFT_SCREEN,
+    RIGHT_SCREEN,
+    LAUNCH_RAYCAST,
+  };
+
+#define NUMS   MO(_NUMS)
+#define LOWER  MO(_LOWER)
+#define RAISE  MO(_RAISE)
+#define TRACK  MO(_TRACK)
+#define MACRO  MO(_MACRO)
+#define ADJUST MO(_ADJUST)
+
+#define L1_TGL  TG(NUMS)
+#define L2_EN   LT(LOWER, KC_LNG2)
+#define L3_JP   LT(RAISE, KC_LNG1)
+#define L3_0    LT(RAISE, KC_0)
+#define L5_BTN2 LT(MACRO, MS_BTN2)
+#define L5_ESC  LT(MACRO, KC_ESC)
+#define L6_EN   LT(ADJUST, KC_LNG2)
+#define L6_JP   LT(ADJUST, KC_LNG1)
+#define ALT_BKSP RALT_T(KC_BSPC)
+#define SHT_MINS RSFT_T(KC_MINS)
+#define SHT_SPC  LSFT_T(KC_SPC)
+#define CTL_ENT  RCTL_T(KC_ENT)
+
+#define PREV_H  PREV_HISTORY
+#define NEXT_H  NEXT_HISTORY
+#define PREV_S  PREV_SCREEN
+#define NEXT_S  NEXT_SCREEN
+#define M_CTRL  MISSION_CONTROL
+#define S_DESK  SHOW_DESKTOP
+#define F_SCR   FULL_SCREEN
+#define C_SCR   CENTER_SCREEN
+#define L_SCR   LEFT_SCREEN
+#define R_SCR   RIGHT_SCREEN
+#define RAYCAST LAUNCH_RAYCAST
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_QWERTY] = LAYOUT(
+        KC_Q,   KC_W,   KC_E,    KC_R,   KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,
+        KC_A,   KC_S,   KC_D,    KC_F,   KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,   ALT_BKSP,
+        KC_Z,   KC_X,   KC_C,    KC_V,   KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT, SHT_MINS,
+                        KC_LGUI, L2_EN,  SHT_SPC,  L5_BTN2, MS_BTN1,  CTL_ENT, L3_JP,   L5_ESC
     ),
-    [1] = LAYOUT(
-        KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,          KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_ESC,  KC_H,    KC_J,    KC_K,    KC_L,    KC_MINS,
-        KC_GRV,  KC_TILD, KC_NUBS, KC_PIPE, XXXXXXX,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                          KC_LALT, KC_LNG2, KC_SPC,  MS_BTN1, KC_ENT,  KC_LNG1, KC_BSPC
+    [_NUMS] = LAYOUT(
+        KC_TAB,  XXXXXXX, KC_LALT, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_7,   KC_8,   KC_9,   KC_DEL,
+        KC_LCTL, LOWER,   L1_TGL,  NUMS,    XXXXXXX,                   KC_DOT,  KC_4,   KC_5,   KC_6,   KC_BSPC,
+        KC_LSFT, KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_1,   KC_2,   KC_3,   SHT_MINS,
+                           KC_LGUI, L2_EN,  _______, XXXXXXX, XXXXXXX, _______, L3_0,   KC_COMM
     ),
-    [2] = LAYOUT(
-        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_ESC,  KC_APP,  KC_UP,   KC_EQL,  KC_PLUS, KC_MINS,
-        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,           KC_LEFT, KC_DOWN, KC_RGHT, KC_DOT,  KC_SLSH,
-                          KC_LALT, KC_LNG2, KC_SPC,  MS_BTN1, KC_ENT,  KC_LNG1, KC_BSPC
+    [_LOWER] = LAYOUT(
+        KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_SLSH, KC_QUES,
+        XXXXXXX, XXXXXXX, XXXXXXX, NUMS,    KC_BSLS,                   KC_GRV,  KC_MINS, KC_EQL,  KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_LALT, XXXXXXX, RAYCAST, KC_PIPE,                   KC_TILD, KC_UNDS, KC_PLUS, KC_COLN, KC_DQUO,
+                          KC_LGUI, L2_EN,   KC_SPC,  XXXXXXX,XXXXXXX,  KC_ENT, L6_JP,   KC_BSLS
     ),
-    [3] = LAYOUT(
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_ESC,  KC_H,    KC_J,    KC_K,    KC_L,    KC_MINS,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                          KC_LALT, KC_LNG2, KC_SPC,  MS_BTN1, KC_ENT,  KC_LNG1, KC_BSPC
+    [_RAISE] = LAYOUT(
+        KC_TAB,  KC_BTN1, KC_MS_U, KC_BTN2, KC_LPRN,                    KC_RPRN, KC_HOME, KC_UP,   KC_END,  KC_DEL,
+        KC_LCTL, KC_MS_L, KC_MS_D, KC_MS_R, KC_LBRC,                    KC_RBRC, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC,
+        KC_LSFT, KC_LALT, XXXXXXX, XXXXXXX, KC_LCBR,                    KC_RCBR, XXXXXXX, KC_PGUP, KC_PGDN, _______,
+                           KC_LGUI, L6_EN,   KC_SPC,  XXXXXXX, XXXXXXX, KC_ENT,  L3_JP,  XXXXXXX
     ),
-    [4] = LAYOUT(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          MS_BTN3, MS_BTN2, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MS_BTN1, SCRL_MO, XXXXXXX
+    [_TRACK] = LAYOUT(
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   MS_BTN3, MS_BTN2, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                          KC_LGUI, SCRL_MO, SHT_SPC, SCRL_MO, MS_BTN1, CTL_ENT, XXXXXXX, KC_ESC
     ),
-    [5] = LAYOUT(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX    ),
-    [6] = LAYOUT(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX    ),
+    [_MACRO] = LAYOUT(
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   M_CTRL, S_DESK, F_SCR,   C_SCR,  XXXXXXX,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     PREV_H, NEXT_H, L_SCR,   R_SCR,  XXXXXXX,
+        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,                    KC_F11,  KC_F12, PREV_S, NEXT_S, KC_RSFT,
+                          KC_LGUI, XXXXXXX, SHT_SPC, XXXXXXX, XXXXXXX, CTL_ENT, XXXXXXX, L5_ESC
+    ),
+    [_ADJUST] = LAYOUT(
+        XXXXXXX, XXXXXXX, RGB_TOG, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, L1_TGL,  KC_CAPS, XXXXXXX,                   XXXXXXX, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_BRID, KC_BRIU, XXXXXXX, XXXXXXX,
+                          XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    ),
     [7] = LAYOUT(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX    )
+                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    )
 };
 
 
@@ -143,3 +201,120 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 
 #endif
 
+
+bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case L2_EN:
+        case L3_JP:
+        case L3_0:
+        case L5_BTN2:
+        case L6_EN:
+        case L6_JP:
+        case ALT_BKSP:
+        case SHT_MINS:
+        case SHT_SPC:
+        case CTL_ENT:
+            // Imediately select the hold action when another key is pressed.
+            return true;
+        default:
+            // Do not select the hold action when another key is pressed.
+            return false;
+    }
+}
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case PREV_H:
+            if (record->event.pressed) {
+                register_code(KC_LGUI);
+                tap_code(KC_LBRC);
+                unregister_code(KC_LGUI);
+            }
+            return false;
+        case NEXT_H:
+            if (record->event.pressed) {
+                register_code(KC_LGUI);
+                tap_code(KC_RBRC);
+                unregister_code(KC_LGUI);
+            }
+            return false;
+        case PREV_S:
+            if (record->event.pressed) {
+                register_code(KC_LCTL);
+                tap_code(KC_COMM);
+                unregister_code(KC_LCTL);
+            }
+            return false;
+        case NEXT_S:
+            if (record->event.pressed) {
+                register_code(KC_LCTL);
+                tap_code(KC_DOT);
+                unregister_code(KC_LCTL);
+            }
+            return false;
+        case M_CTRL:
+            if (record->event.pressed) {
+                register_code(KC_LALT);
+                register_code(KC_LCTL);
+                tap_code(KC_UP);
+                unregister_code(KC_LALT);
+                unregister_code(KC_LCTL);
+            }
+            return false;
+        case S_DESK:
+            if (record->event.pressed) {
+                register_code(KC_LALT);
+                register_code(KC_LCTL);
+                tap_code(KC_DOWN);
+                unregister_code(KC_LALT);
+                unregister_code(KC_LCTL);
+            }
+            return false;
+        case F_SCR:
+            if (record->event.pressed) {
+                register_code(KC_LALT);
+                register_code(KC_LGUI);
+                tap_code(KC_ENT);
+                unregister_code(KC_LALT);
+                unregister_code(KC_LGUI);
+            }
+            return false;
+        case C_SCR:
+            if (record->event.pressed) {
+                register_code(KC_LALT);
+                register_code(KC_LGUI);
+                tap_code(KC_SLSH);
+                unregister_code(KC_LALT);
+                unregister_code(KC_LGUI);
+            }
+            return false;
+        case L_SCR:
+            if (record->event.pressed) {
+                register_code(KC_LALT);
+                register_code(KC_LGUI);
+                tap_code(KC_LEFT);
+                unregister_code(KC_LALT);
+                unregister_code(KC_LGUI);
+            }
+            return false;
+        case R_SCR:
+            if (record->event.pressed) {
+                register_code(KC_LALT);
+                register_code(KC_LGUI);
+                tap_code(KC_RGHT);
+                unregister_code(KC_LALT);
+                unregister_code(KC_LGUI);
+            }
+            return false;
+        case RAYCAST:
+            if (record->event.pressed) {
+                register_code(KC_LALT);
+                tap_code(KC_SPC);
+                unregister_code(KC_LALT);
+            }
+            return false;
+        default:
+            return true;
+    }
+}
